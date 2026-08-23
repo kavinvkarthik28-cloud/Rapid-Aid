759713705ca4c2f4b39a05e4b90f90cdd733bb57
Kavin Karthik V <kavin.v.karthik.12.24@gmail.com>
2026-08-23 21:49:14 +0530

    Switch map to CartoDB Voyager for better readability

--- public/js/dispatcher.js
+++ public/js/dispatcher.js
@@ -2,7 +2,7 @@ const socket = io({ transports: ["polling"] });
 
 // Coimbatore default center
 const map = L.map("map").setView([11.0168, 76.9558], 13);
-L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
   attribution: "© OpenStreetMap contributors, © CartoDB",
   subdomains: "abcd",
   maxZoom: 20
